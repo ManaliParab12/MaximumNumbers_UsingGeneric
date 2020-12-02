83d98a32817f54aed56b34ef17ea67a53aaa8ede
ManaliParab12 <manaliparab10@gmail.com>
2020-12-02 17:23:55 +0530

    Welcome to Test Maximum Number using Generic

---
+++ main/java/com/maximumnumber/MaximumNumber.java
@@ -0,0 +1,7 @@
+package com.maximumnumber;
+
+public class MaximumNumber {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Test Maximum Number using Generic");
+    }
+}
