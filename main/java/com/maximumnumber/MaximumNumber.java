31084b1dd4ca081679024daeb5ac7dbbb573f70d
ManaliParab12 <manaliparab10@gmail.com>
2020-12-03 14:08:57 +0530

    Refactored the code to Create generic class and assigned 3 variables of generic type

--- main/java/com/maximumnumber/MaximumNumber.java
+++ main/java/com/maximumnumber/MaximumNumber.java
@@ -1,12 +1,21 @@
 package com.maximumnumber;
 
-public class MaximumNumber {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Test Maximum Number using Generic");
-        MaximumNumber maximumnumber = new MaximumNumber();
+public class MaximumNumber <T extends Comparable>  {
+    private T value1;
+    private T value2;
+    private T value3;
+
+    public MaximumNumber (T value1, T value2, T value3) {
+        this.value1 = value1;
+        this.value2 = value2;
+        this.value3 = value3;
     }
 
-    public static<T extends Comparable <T>> T checkMaximumValue(T value1, T value2, T value3){
+    public T checkMaximumValue(){
+        return MaximumNumber.checkMaximumValue(value1, value2, value3);
+    }
+
+    public static <T extends Comparable> T checkMaximumValue(T value1, T value2, T value3) {
         T maximumValue = value1;
         if (value2.compareTo(maximumValue) > 0) {
             maximumValue = value2;
@@ -18,4 +27,7 @@ public class MaximumNumber {
         return maximumValue;
     }
 
+    public static void main(String[] args) {
+        System.out.println("Welcome to Test Maximum Number using Generic");
+    }
 }
--- test/java/com/maximumnumber/NumberTest.java
+++ test/java/com/maximumnumber/NumberTest.java
@@ -4,67 +4,75 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class NumberTest {
-    MaximumNumber maximumnumber = new MaximumNumber();
 
     @Test
     public void givenMaximumInteger_whenAtFirstPosition_shouldReturnSameNumber(){
-        Integer actualMaximumInteger = maximumnumber.checkMaximumValue(42, 23, 1);
+        MaximumNumber maximumnumber = new MaximumNumber(42, 23, 1);
+        Integer actualMaximumInteger = (Integer) maximumnumber.checkMaximumValue();
         Integer expectedMaximumNumber = 42;
         Assert.assertEquals(expectedMaximumNumber, actualMaximumInteger);
     }
 
     @Test
     public void givenMaximumInteger_whenAtSecondPosition_shouldReturnSameNumber(){
-        Integer actualMaximumInteger = maximumnumber.checkMaximumValue(1, 42, 23);
+        MaximumNumber maximumnumber = new MaximumNumber(1, 42, 23);
+        Integer actualMaximumInteger = (Integer) maximumnumber.checkMaximumValue();
         Integer expectedMaximumNumber = 42;
         Assert.assertEquals(expectedMaximumNumber, actualMaximumInteger);
     }
 
     @Test
     public void givenMaximumInteger_whenAtThirdPosition_shouldReturnSameNumber(){
-        Integer actualMaximumInteger = maximumnumber.checkMaximumValue(23, 1, 42);
+        MaximumNumber maximumnumber = new MaximumNumber(23, 1, 42);
+        Integer actualMaximumInteger = (Integer) maximumnumber.checkMaximumValue();
         Integer expectedMaximumNumber = 42;
         Assert.assertEquals(expectedMaximumNumber, actualMaximumInteger);
     }
 
     @Test
     public void givenMaximumFloat_whenAtFirstPosition_shouldReturnSameNumber(){
-        Float actualMaximumFloat = maximumnumber.checkMaximumValue(40.44f, 12.7f, 2.54f );
+        MaximumNumber maximumnumber = new MaximumNumber(40.44f, 12.7f, 2.54f);
+        Float actualMaximumFloat = (Float) maximumnumber.checkMaximumValue();
         Float expectedMaximumNumber = 40.44f;
         Assert.assertEquals(expectedMaximumNumber, actualMaximumFloat);
     }
 
     @Test
     public void givenMaximumIFloat_whenAtSecondPosition_shouldReturnSameNumber(){
-        Float actualMaximumFloat = maximumnumber.checkMaximumValue( 2.54f, 40.44f, 12.7f);
+        MaximumNumber maximumnumber = new MaximumNumber(2.54f, 40.44f, 12.7f);
+        Float actualMaximumFloat = (Float) maximumnumber.checkMaximumValue();
         Float expectedMaximumNumber = 40.44f;
         Assert.assertEquals(expectedMaximumNumber, actualMaximumFloat);
     }
 
     @Test
     public void givenMaximumFloat_whenAtThirdPosition_shouldReturnSameNumber(){
-        Float actualMaximumFloat = maximumnumber.checkMaximumValue(12.7f, 2.54f, 40.44f);
+        MaximumNumber maximumnumber = new MaximumNumber(12.7f, 2.54f, 40.44f);
+        Float actualMaximumFloat = (Float) maximumnumber.checkMaximumValue();
         Float expectedMaximumNumber = 40.44f;
         Assert.assertEquals(expectedMaximumNumber, actualMaximumFloat);
     }
 
     @Test
     public void givenMaximumString_whenAtFirstPosition_shouldReturnSameValue(){
-        String actualMaximumString = maximumnumber.checkMaximumValue("Peach", "Banana", "Apple");
+        MaximumNumber maximumnumber = new MaximumNumber("Peach", "Banana", "Apple");
+        String actualMaximumString = (String) maximumnumber.checkMaximumValue();
         String expectedMaximumNumber = "Peach";
         Assert.assertEquals(expectedMaximumNumber, actualMaximumString);
     }
 
     @Test
     public void givenMaximumIString_whenAtSecondPosition_shouldReturnSameValue(){
-        String actualMaximumString = maximumnumber.checkMaximumValue( "Apple", "Peach", "Banana");
+        MaximumNumber maximumnumber = new MaximumNumber("Apple", "Peach", "Banana");
+        String actualMaximumString = (String) maximumnumber.checkMaximumValue();
         String expectedMaximumNumber = "Peach";
         Assert.assertEquals(expectedMaximumNumber, actualMaximumString);
     }
 
     @Test
     public void givenMaximumString_whenAtThirdPosition_shouldReturnSameValue(){
-        String actualMaximumString = maximumnumber.checkMaximumValue("Banana", "Apple", "Peach" );
+        MaximumNumber maximumnumber = new MaximumNumber("Banana", "Apple", "Peach");
+        String actualMaximumString = (String) maximumnumber.checkMaximumValue();
         String expectedMaximumNumber = "Peach";
         Assert.assertEquals(expectedMaximumNumber, actualMaximumString);
     }
